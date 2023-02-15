0b2db759dd2e1bef4263203c4c6cc53b8336a184
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-15 17:57:13 -0600

    ISCLAB

--- ISCLAB/src/components/HelloWorld.vue
+++ ISCLAB/src/components/HelloWorld.vue
@@ -9,9 +9,9 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">ISCLAB</h1>
+    <h1 class="green">{{ msg }}</h1>
     <h3>
-      Laboratorio de sistemas y aplicaciones web
+      You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
