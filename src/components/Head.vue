efb20841bd8e376ba3adce74110ba7d9732d23cf
Emiliano530 <124843074+Emiliano530@users.noreply.github.com>
2023-06-18 03:38:33 -0600

    Add files via upload

---
+++ src/components/Head.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div class="greetings">
+      <h3>
+        Laboratorio de sistemas y aplicaciones web
+        <br>
+        <a href="contacto@isclab.com.mx" target="_blank" rel="noopener">contactanos
+        </a>.
+      </h3>
+    </div>
+  </template>
+  
+  <style scoped>
+  h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+  }
+  
+  h3 {
+    font-size: 1.2rem;
+  }
+  
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+  
+  @media (min-width: 1024px) {
+    .greetings h1,
+    .greetings h3 {
+      text-align: left;
+    }
+  }
+  </style>
+  
